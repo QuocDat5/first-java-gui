2de431bfe96634aecba9428b6e8ef68a7f6cc8f2
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-06-28 21:52:41 +0700

    process the button click

--- src/GUI.java
+++ src/GUI.java
@@ -5,28 +5,41 @@ import javax.swing.JLabel;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 
 
 
-public class GUI {
+public class GUI implements ActionListener {
+	
+	private int count = 0;
+	
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel;
+	
+	
 	
 	public GUI() {
 		
-		JFrame frame = new JFrame();
+		frame = new JFrame();
 		
+		// the clickable button
 		JButton button = new JButton("Click me!");
+		button.addActionListener(this);
 		
-		JLabel label = new JLabel("Number of clicks: 0");
+		label = new JLabel();
 		
-		JPanel panel = new JPanel();
+        // the panel with the button and text
+		panel = new JPanel();
 		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
 		panel.setLayout(new GridLayout(0, 1));
 		panel.add(button);
 		panel.add(label);
 		
+        // set up the frame and display it
 		frame.add(panel, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Dat's first Java GUI");
@@ -37,6 +50,15 @@ public class GUI {
 
 	public static void main(String[] args) {
 		new GUI();
+	}
+
+	@Override
+    // process the button clicks
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		count++;
+		
+		label.setText("Number of clicks: " + count);
 	}
 
 }
