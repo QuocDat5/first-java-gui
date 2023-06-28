c7d215775fae32329ed8487a0f2abde350b739f2
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-06-28 21:26:53 +0700

    added button & label

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,11 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+
 
 import javax.swing.BorderFactory;
 
@@ -14,9 +17,15 @@ public class GUI {
 		
 		JFrame frame = new JFrame();
 		
+		JButton button = new JButton("Click me!");
+		
+		JLabel label = new JLabel("Number of clicks: 0");
+		
 		JPanel panel = new JPanel();
 		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
 		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		panel.add(label);
 		
 		frame.add(panel, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
